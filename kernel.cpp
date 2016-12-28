69aa23e13656a23397d96a43972720242bbc051d
Filip Miletic <filip.miletic@me.com>
2016-12-28 12:23:00 +0100

    updt

--- kernel.cpp
+++ kernel.cpp
@@ -5,8 +5,17 @@ void printf(char*str)
     for(int i=0; str[i] != '\0'; ++i)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
-    
-void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for (constructor* i = &start_ctors; i != end_ctors; i++)
+        (*i)();
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
     printf("Hello World!");
 
