df64964d16aaebf174aa1139b6188371be4cb76e
Filip Miletic <filip.miletic@me.com>
2017-01-01 19:20:01 +0100

    fixed test case

--- kernel.cpp
+++ kernel.cpp
@@ -43,8 +43,8 @@ void printf(char*str)
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t  /*magicnumber*/)
 {
-    printf("Hello World! TEST TEST TEST TEST TEST");
-    printf("Hello World! TEST TEST TEST TEST TEST");
+    printf("Hello World! TEST TEST TEST TEST TEST TEST TEST TEST TEST");
+    printf("Hello World! TEST TEST TEST TEST TEST TEST TEST TEST TEST");
 
     GlobalDescriptorTable gdt;
 
